c124d35e5e1f5a81db4fb797e1708bc5e2663256
ChildishWu <david.a.malcolms@gmail.com>
2023-10-24 23:33:01 -0500

    Added sidebar functionality and labelled machine icons

--- laundry.js
+++ laundry.js
@@ -1,3 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const timeSlots = document.querySelectorAll(".timeSlot");
+    const menu = document.getElementById("menu");
+    const side = document.getElementById("sidebar")
+    const close = document.getElementById("close")
+
+    function slideBar(){
+        side.style.left = "0";
+    }
+
+    function closer(){
+        side.style.left ="-300px";
+    }
+
+    menu.addEventListener("click",slideBar)
+
+    close.addEventListener("click",closer)
+    timeSlots.forEach( slot => {
+        slot.addEventListener("click",function(){
+            this.classList.toggle('selected');
+            
+            //const selectedSlots = Array.from(timeSlots).filter(slot => slot.classList.contains('selected')).map()
+        })    
+    });
+});
+
+
+
+
 function updateLocalTime(){
     const timeNow = new Date();
     const current = timeNow.toLocaleString();
@@ -6,3 +35,6 @@ function updateLocalTime(){
 }
 
 setInterval(updateLocalTime,1000);
+
+
+
