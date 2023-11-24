7ebbaaba5dc9f2194b282c532c9b94fccd434a25
ChildishWu <david.a.malcolms@gmail.com>
2023-11-23 20:49:19 -0500

    Current Page script

--- laundry.js
+++ laundry.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const side = document.getElementById("sidebar")
     const close = document.getElementById("close")
     const menuItems = document.querySelectorAll(".sideLinks");
+    const menuItem = document.querySelectorAll(".sideLinks a")
     let currLocation = window.location.pathname;
     
-    console.log(currLocation);
     
     if(currLocation == "/base.html"){
         menuItems[0].classList.toggle('selected');
+        menuItem[0].classList.toggle('selected')
     }
 
     function slideBar(){
