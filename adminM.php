4abdbd77235d1b6e298b7be82b0045ed253dedc5
Sharian Johnson <114778798+sharian43@users.noreply.github.com>
2023-11-28 19:25:21 -0500

    Create adminM.php

---
+++ adminM.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Panel</title>
+  <link rel="stylesheet" href="maint.css">
+</head>
+<body>
+
+  <h2>Admin Panel - Submitted Issues</h2>
+
+  <?php
+  // Read and display submitted issues from the file
+  $file = 'submittedIssues.txt';
+
+  if (file_exists($file)) {
+    $submittedIssues = file_get_contents($file);
+    echo "<p>Submitted Issues:</p>";
+    echo "<pre>$submittedIssues</pre>";
+  } else {
+    echo "<p>No submitted issues yet.</p>";
+  }
+  ?>
+
+</body>
+</html>
