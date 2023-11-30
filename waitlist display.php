0cfaabef6e659c39c71455d90187fa78437fce6b
ChildishWu <david.a.malcolms@gmail.com>
2023-11-29 19:45:12 -0500

    minimum viable change to waitlst

--- waitlist display.php
+++ waitlist display.php
@@ -4,14 +4,56 @@ $user = $_SESSION['userName'];
 
 $mysqli = new mysqli("localhost", "root", "", "138users");
 
-
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
-$query = mysql->prepare("SELECT id, machine, day FROM reservations WHERE user_name=?");
 
+$currentDay = date('w');
+$currentHour = date('H');
+
+
+$query = $mysqli->prepare("SELECT id, machine, timeslot, user_name FROM reservations WHERE user_name = ? AND day = ?");
+$query->bind_param("ss", $user, $currentDay);
+
+if ($query->execute()) {
+    $result = $query->get_result();
+    if ($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        $ticketNumber = $row["id"];
+    }
+    else{
+    }
+
+}
+
+
+$machineQuery = $mysqli->prepare("SELECT id, machine, timeslot, day, user_name FROM reservations WHERE machine = ? AND day = ? ORDER BY ABS(TIMESTAMPDIFF(MINUTE, timeslot, ?))");
+$currentHourRef = &$currentHour; 
+$machineQuery->bind_param("sss", $row["machine"], $currentDay, $currentHourRef);
+
+if ($machineQuery->execute()) {
+    $result = $machineQuery->get_result();
+    $waitlist = array();
+    $nowServing = array();
+    while ($rows = $result->fetch_assoc()) {
+        $status = ($rows['user_name'] === $user) ? "Now Serving" : "In Waiting";
+        $entry = array(
+            'ticketNumber' => $rows['id'],
+            'name' => $rows['user_name'],
+            'machine' => $rows['machine'],
+            'status' => $status,
+        );
+
+        if ($status === "Now Serving") {
+            $nowServing[] = $entry;
+        } else {
+            $waitlist[] = $entry;
+        }
+    }
+}
 ?>
+
 
 
 <!DOCTYPE html>
@@ -39,7 +81,16 @@ $query = mysql->prepare("SELECT id, machine, day FROM reservations WHERE user_na
                     <th>Status</th>
                 </tr>
             </thead>
-            <tbody id="serving"></tbody>
+            <tbody id="serving">
+                <?php foreach ($nowServing as $entry):?>
+                    <tr>
+                        <td><?=$entry['ticketNumber']?></td>
+                        <td><?=$entry['name']?></td>
+                        <td><?=$entry['machine']?></td>
+                        <td><?=$entry['status']?></td>
+                    </tr>
+                <?php endforeach;?>
+            </tbody>
         </table>
        
         <h3>IN - WAITING</h3>
@@ -52,11 +103,17 @@ $query = mysql->prepare("SELECT id, machine, day FROM reservations WHERE user_na
                     <th>Status</th>
                 </tr>
             </thead>
-            <tbody id="Waitlist"></tbody>
+            <tbody id="Waitlist">
+                <?php foreach($waitlist as $entry):?>
+                    <tr>
+                        <td><?=$entry['ticketNumber']?></td>
+                        <td><?=$entry['name']?></td>
+                        <td><?=$entry['machine']?></td>
+                        <td><?=$entry['status']?></td>
+                    </tr>
+                <?php endforeach;?>
+            </tbody>
         </table>
-
     </div>
-
-   
 </body>
 </html>
